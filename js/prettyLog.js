b2660ad93625c8375286dd688d7d98cf70705880
Scott Palomino <spalomino@salesforce.com>
2014-04-01 16:38:42 -0700

    Switched colored text with lightly colored divs to indicate stack hierarchy. SOQL statements and user debug statements are now bolded.

--- js/prettyLog.js
+++ js/prettyLog.js
@@ -1,5 +1,5 @@
 function init() {
-    var colors = ['000000', 'A10000', '1BA100', '26B6DE', '003EA1', '814AE0'];
+    var colors = ['FFFAFA', 'FFDDBF', 'FFEFBF', 'FFFABF', 'FFFFBF', 'EAFFBF'];
     var colorsLength = colors.length;
     var cIndex = 0;
     var pre = jQuery('pre').text();
@@ -25,18 +25,22 @@ function init() {
     var length = lines.length;
     for (var i = 0; i < length; i++) {
         if (lines[i].indexOf('|METHOD_ENTRY') !== -1) {
-            shouldDecrement = false;
+            lines[i] = '<div style="background-color:#' + colors[cIndex] + ';"><pre>' + lines[i] + '\n';
             cIndex = incrementCIndex(cIndex, colorsLength);
-            lines[i] = '<div style="color:#' + colors[cIndex] + ';padding-left:25px"><pre>' + lines[i] + '\n';
         } else if (lines[i].indexOf('|METHOD_EXIT') !== -1)  {
             cIndex = decrementCIndex(cIndex, colorsLength);
             lines[i] = lines[i] + '</pre></div>'
+        } else if (lines[i].indexOf('|USER_DEBUG|') !== -1 || lines[i].indexOf('|SOQL_EXECUTE') !== -1)  {
+            lines[i] = '<b>' + lines[i] + '</b>\n'
         } else {
             lines[i] = lines[i] + '\n';
         } 
     }
 
-    jQuery('body').css('white-space', 'pre').append(lines.join(''));
+    jQuery('body').css({
+        'white-space': 'pre',
+        'font-family': 'monospace'
+    }).append('<link rel="stylesheet" type="text/css" href="#" />').append(lines.join(''));
 
     function incrementCIndex(current, len) {
         var result = current+1;
